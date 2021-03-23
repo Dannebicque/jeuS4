bad6add4ab99b842b931c66359c921ccd859403e
dannebicque <david.annebicque@gmail.com>
2021-03-23 11:29:51 +0100

    EasyAdmin et mailer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -16,5 +18,23 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    /**
+     * @Route("/mail", name="send_mail")
+     */
+    public function sendMail(MailerInterface $mailer)
+    {
+        $email = (new TemplatedEmail())
+            ->to('david.annebicque@gmail.com')
+            ->from('david.annebicque@univ-reims.fr')
+            ->subject('Test d un mail')
+            ->htmlTemplate('mail/mail.html.twig')
+            ->context([
+                'prenom' => 'david',
+                'nom' => 'Annebicque',
+            ]);
+
+        $mailer->send($email);
     }
 }
